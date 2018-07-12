c38e56aa4b959ab4d98bb4558c49a5b602eb08e6
Mr-BlackPY-l1n0xw1nd0ws <40911447+Mr-BlackPY-l1n0xw1nd0ws@users.noreply.github.com>
2018-07-12 23:03:20 +0430

    Create Passwordgenerator.py

---
+++ Passwordgenerator.py
@@ -0,0 +1,23 @@
+  import string
+
+from random import *
+#startMenu
+print("---------------------------------")
+startMenu = (" password generator \n " +
+             "made by: Mr.BlackPY \n "+
+             "Wed Jul 11 ")
+print(startMenu)
+print("---------------------------------")
+
+#password generator
+def passgen():
+    test = string.ascii_letters + string.punctuation + string.digits + string.hexdigits
+    npass = "".join(choice(test)
+                    for x in range(randint(10,20)))
+    return npass
+
+#Result
+print("your password is:")
+print(passgen())
+
+#t.me/Mr_BlackPY
